8e25c040c5bb5e8523391b4fa642f0049e966bf9
Ebisa-Mitiku <ebisamitiku203@gmail.com>
2026-02-17 14:20:29 +0300

    Create Die Roll.py

---
+++ Die Roll.py
@@ -0,0 +1,19 @@
+l=list(map(int,input().split()))
+t=[1,2,3,4,5,6]
+d=max(l)
+count=0
+for i in range(6):
+    if(d<=t[i]):
+        count=count+1    
+if(count==1):
+    print("1/6")
+elif(count==2):
+    print("1/3")
+elif(count==3):
+    print("1/2")
+elif(count==4):
+    print("2/3")
+elif(count==5):
+    print("5/6")
+else:
+    print("1/1")
